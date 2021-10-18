f429b7f2a46574892b813cf1f639394a9d56396f
Palak P Shah <dabbadolcha@gmail.com>
2021-10-19 03:48:32 +0530

    config.yml for 6 circleci

--- features/steps/environment.py
+++ features/steps/environment.py
@@ -15,9 +15,12 @@ user_agent = \
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 PTST/1.0'
 options.add_argument('user-agent={0}'.format(user_agent))
 # use this code below to execute headless state
+
 # driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(),options=options)
+driver = webdriver.chrom(ChromeDriverManager(version="94.0.4606.61").install(),options=options)
+
+# driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(),options=options)
 
 url_name = "https://staging.blavity.com/"
 
