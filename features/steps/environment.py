d32a0fe49cee0c44e4fc937994cdec72715a080e
Palak P Shah <dabbadolcha@gmail.com>
2021-10-19 02:03:31 +0530

    config.yml for 5 circleci

--- features/steps/environment.py
+++ features/steps/environment.py
@@ -1,4 +1,6 @@
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
