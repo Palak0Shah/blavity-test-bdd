074a02d02f591b235e30af33df8f565333892239
Palak P Shah <dabbadolcha@gmail.com>
2021-10-19 05:47:59 +0530

    config.yml for 17 circleci

--- features/steps/environment.py
+++ features/steps/environment.py
@@ -9,6 +9,7 @@ import time
 options = Options()
 options.headless = True
 options.add_argument('--start-maximized')
+options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
 options.add_argument("--headless")
 options.add_argument("--window-size=1920x1080")
