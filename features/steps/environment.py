0992848fb7d6a913af4703295477108538d1b79d
Palak P Shah <dabbadolcha@gmail.com>
2021-10-19 05:52:51 +0530

    config.yml for 18 circleci

--- features/steps/environment.py
+++ features/steps/environment.py
@@ -7,9 +7,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 options = Options()
-options.headless = True
+options.headless = False
 options.add_argument('--start-maximized')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument("disable-infobars")
+options.add_argument("--disable-extensions")
 options.add_argument('--no-sandbox')
 options.add_argument("--headless")
 options.add_argument("--window-size=1920x1080")
