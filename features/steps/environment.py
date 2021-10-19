7d8caf85cb23bca15d38cbb028c313cd17040bdb
Palak P Shah <dabbadolcha@gmail.com>
2021-10-19 05:39:04 +0530

    config.yml for 15 circleci

--- features/steps/environment.py
+++ features/steps/environment.py
@@ -11,6 +11,7 @@ options.headless = True
 options.add_argument('--start-maximized')
 options.add_argument("--headless")
 options.add_argument("--window-size=1920x1080")
+options.binary_location = "ChromeExe/chromedriver_linux64/chromedriver"
 user_agent = \
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 PTST/1.0'
 options.add_argument('user-agent={0}'.format(user_agent))
