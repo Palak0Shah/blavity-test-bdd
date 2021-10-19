f30c86031acf1c9be9535c57227ce7f7aadd67a3
Palak P Shah <dabbadolcha@gmail.com>
2021-10-19 05:30:11 +0530

    config.yml for 13 circleci

--- features/steps/environment.py
+++ features/steps/environment.py
@@ -1,6 +1,6 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
-
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
@@ -17,7 +17,7 @@ options.add_argument('user-agent={0}'.format(user_agent))
 # use this code below to execute headless state
 
 # driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
+os.chmod('ChromeExe/chromedriver_linux64/chromedriver', 0o755)
 driver = webdriver.Chrome("ChromeExe/chromedriver_linux64/chromedriver",options=options)
 
 # driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(),options=options)
